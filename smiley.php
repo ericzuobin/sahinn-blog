b22f019e011afdf84a7899f9db72b8748d9ca333
ericzuobin <ericzuobin@qq.com>
2016-01-28 17:36:39 +0800

    修改静态文件适应CDN

--- smiley.php
+++ smiley.php
@@ -32,24 +32,25 @@
     }
 /* ]]> */
 </script>
-<a href="javascript:grin(':?:')"      ><img src="<?php bloginfo('template_url'); ?>/images/smilies/icon_question.gif"  alt="" /></a>
-<a href="javascript:grin(':razz:')"   ><img src="<?php bloginfo('template_url'); ?>/images/smilies/icon_razz.gif"      alt="" /></a>
-<a href="javascript:grin(':sad:')"    ><img src="<?php bloginfo('template_url'); ?>/images/smilies/icon_sad.gif"       alt="" /></a>
-<a href="javascript:grin(':evil:')"   ><img src="<?php bloginfo('template_url'); ?>/images/smilies/icon_evil.gif"      alt="" /></a>
-<a href="javascript:grin(':!:')"      ><img src="<?php bloginfo('template_url'); ?>/images/smilies/icon_exclaim.gif"   alt="" /></a>
-<a href="javascript:grin(':smile:')"  ><img src="<?php bloginfo('template_url'); ?>/images/smilies/icon_smile.gif"     alt="" /></a>
-<a href="javascript:grin(':oops:')"   ><img src="<?php bloginfo('template_url'); ?>/images/smilies/icon_redface.gif"   alt="" /></a>
-<a href="javascript:grin(':grin:')"   ><img src="<?php bloginfo('template_url'); ?>/images/smilies/icon_biggrin.gif"   alt="" /></a>
-<a href="javascript:grin(':shock:')"  ><img src="<?php bloginfo('template_url'); ?>/images/smilies/icon_eek.gif"       alt="" /></a>
-<a href="javascript:grin(':???:')"    ><img src="<?php bloginfo('template_url'); ?>/images/smilies/icon_confused.gif"  alt="" /></a>
-<a href="javascript:grin(':cool:')"   ><img src="<?php bloginfo('template_url'); ?>/images/smilies/icon_cool.gif"      alt="" /></a>
-<a href="javascript:grin(':lol:')"    ><img src="<?php bloginfo('template_url'); ?>/images/smilies/icon_lol.gif"       alt="" /></a>
-<a href="javascript:grin(':mad:')"    ><img src="<?php bloginfo('template_url'); ?>/images/smilies/icon_mad.gif"       alt="" /></a>
-<a href="javascript:grin(':twisted:')"><img src="<?php bloginfo('template_url'); ?>/images/smilies/icon_twisted.gif"   alt="" /></a>
-<a href="javascript:grin(':roll:')"   ><img src="<?php bloginfo('template_url'); ?>/images/smilies/icon_rolleyes.gif"  alt="" /></a>
-<a href="javascript:grin(':wink:')"   ><img src="<?php bloginfo('template_url'); ?>/images/smilies/icon_wink.gif"      alt="" /></a>
-<a href="javascript:grin(':idea:')"   ><img src="<?php bloginfo('template_url'); ?>/images/smilies/icon_idea.gif"      alt="" /></a>
-<a href="javascript:grin(':arrow:')"  ><img src="<?php bloginfo('template_url'); ?>/images/smilies/icon_arrow.gif"     alt="" /></a>
-<a href="javascript:grin(':neutral:')"><img src="<?php bloginfo('template_url'); ?>/images/smilies/icon_neutral.gif"   alt="" /></a>
-<a href="javascript:grin(':cry:')"    ><img src="<?php bloginfo('template_url'); ?>/images/smilies/icon_cry.gif"       alt="" /></a>
-<a href="javascript:grin(':mrgreen:')"><img src="<?php bloginfo('template_url'); ?>/images/smilies/icon_mrgreen.gif"   alt="" /></a>
+<?php /*重定向以前使用,<?php bloginfo('template_url'); ?>,在博客目录下创建一个source软连 */ ?>
+<a href="javascript:grin(':?:')"      ><img src="<?php sahinn_blog_source(); ?>/images/smilies/icon_question.gif"  alt="" /></a>
+<a href="javascript:grin(':razz:')"   ><img src="<?php sahinn_blog_source(); ?>/images/smilies/icon_razz.gif"      alt="" /></a>
+<a href="javascript:grin(':sad:')"    ><img src="<?php sahinn_blog_source(); ?>/images/smilies/icon_sad.gif"       alt="" /></a>
+<a href="javascript:grin(':evil:')"   ><img src="<?php sahinn_blog_source(); ?>/images/smilies/icon_evil.gif"      alt="" /></a>
+<a href="javascript:grin(':!:')"      ><img src="<?php sahinn_blog_source(); ?>/images/smilies/icon_exclaim.gif"   alt="" /></a>
+<a href="javascript:grin(':smile:')"  ><img src="<?php sahinn_blog_source(); ?>/images/smilies/icon_smile.gif"     alt="" /></a>
+<a href="javascript:grin(':oops:')"   ><img src="<?php sahinn_blog_source(); ?>/images/smilies/icon_redface.gif"   alt="" /></a>
+<a href="javascript:grin(':grin:')"   ><img src="<?php sahinn_blog_source(); ?>/images/smilies/icon_biggrin.gif"   alt="" /></a>
+<a href="javascript:grin(':shock:')"  ><img src="<?php sahinn_blog_source(); ?>/images/smilies/icon_eek.gif"       alt="" /></a>
+<a href="javascript:grin(':???:')"    ><img src="<?php sahinn_blog_source(); ?>/images/smilies/icon_confused.gif"  alt="" /></a>
+<a href="javascript:grin(':cool:')"   ><img src="<?php sahinn_blog_source(); ?>/images/smilies/icon_cool.gif"      alt="" /></a>
+<a href="javascript:grin(':lol:')"    ><img src="<?php sahinn_blog_source(); ?>/images/smilies/icon_lol.gif"       alt="" /></a>
+<a href="javascript:grin(':mad:')"    ><img src="<?php sahinn_blog_source(); ?>/images/smilies/icon_mad.gif"       alt="" /></a>
+<a href="javascript:grin(':twisted:')"><img src="<?php sahinn_blog_source(); ?>/images/smilies/icon_twisted.gif"   alt="" /></a>
+<a href="javascript:grin(':roll:')"   ><img src="<?php sahinn_blog_source(); ?>/images/smilies/icon_rolleyes.gif"  alt="" /></a>
+<a href="javascript:grin(':wink:')"   ><img src="<?php sahinn_blog_source(); ?>/images/smilies/icon_wink.gif"      alt="" /></a>
+<a href="javascript:grin(':idea:')"   ><img src="<?php sahinn_blog_source(); ?>/images/smilies/icon_idea.gif"      alt="" /></a>
+<a href="javascript:grin(':arrow:')"  ><img src="<?php sahinn_blog_source(); ?>/images/smilies/icon_arrow.gif"     alt="" /></a>
+<a href="javascript:grin(':neutral:')"><img src="<?php sahinn_blog_source(); ?>/images/smilies/icon_neutral.gif"   alt="" /></a>
+<a href="javascript:grin(':cry:')"    ><img src="<?php sahinn_blog_source(); ?>/images/smilies/icon_cry.gif"       alt="" /></a>
+<a href="javascript:grin(':mrgreen:')"><img src="<?php sahinn_blog_source(); ?>/images/smilies/icon_mrgreen.gif"   alt="" /></a>
